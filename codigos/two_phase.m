fc05d476f7a63c63b611a9d12e7bf2219ff09bfc
Feraul <ferlicapac@gmail.com>
2016-10-02 09:28:22 -0300

    add comentario

--- codigos/two_phase.m
+++ codigos/two_phase.m
@@ -14,12 +14,12 @@ global coord centelem elem esurn1 esurn2 nsurn1 nsurn2 bedge inedge ...
     satlimit,pormap,bcflag,courant,totaltime,filepath,foldername,kmap,...
     wells] = preprocessor;
 %[auxcoord]=distortedramd;
-%% só em malha com furo
+%% so em malha com furo
 % x=bedge(129:144,1);
 % y=bedge(129:144,2);
 % bedge(129:144,1)=y;
 % bedge(129:144,2)=x;
-%% só em malha "Tipo1malha1"
+%% so em malha "Tipo1malha1"
 % x=bedge(:,1);
 % y=bedge(:,2);
 % bedge(:,1)=y;
@@ -48,7 +48,7 @@ global coord centelem elem esurn1 esurn2 nsurn1 nsurn2 bedge inedge ...
 %bedge(135:150,4:5)=102; % 36x36
 %bcflag(2,1)=102;
 %bcflag(2,2)=2;
-%% Modificação Malha Kershaw
+%% Modificaï¿½ï¿½o Malha Kershaw
 %bedge(:,4:5)=101;
 %----------------------------
 %% tratamento malha Hermeline
@@ -75,18 +75,18 @@ global coord centelem elem esurn1 esurn2 nsurn1 nsurn2 bedge inedge ...
 %bedge(128:192,2)=x;
 %%
 tic
-%% escolha o método de interpolação 
+%% escolha o mï¿½todo de interpolaï¿½ï¿½o
 % 1-->LPEW1 Gao e Wu 2010
 % 2-->LPEW2 Gao e Wu 2010
 interptype=2;
-% numérodo de Courant
+% numero de Courant
 CFL=courant;
 %% exponente das permeabilidades relativas cuidado...! que varia com o benchmark
 nw=2;
 no=2;
-%% Digite 
-% monofasico ---> quando deseje rodar um problema de escoamento monofásico ou
-% bifasico   ---> quando deseja rodar um problema de ecoamento bifásico 
+%% Digite
+% monofasico ---> quando deseje rodar um problema de escoamento monofï¿½sico ou
+% bifasico   ---> quando deseja rodar um problema de ecoamento bifï¿½sico
 simu='bifasico';
 %% escolha o tipo de erro discreto que deseja usar
 % erromethod1 ---> erro utilizado por Gao e Wu 2010
@@ -94,44 +94,44 @@ simu='bifasico';
 % erromethod3 --->  ''     ''     por Eigestad et al 2005
 % erromethod4 --->  ''     ''     por Shen e Yuan 2015
 erromethod='erromethod1';
-%% Defina o tipo de solver de pressão
-% mpfad--> método linear dos volumes finitos baseado no Gao e Wu 2010
-% tpfa ---> método Linear dos volumes finito 
-% nlfvLPEW --> método não linear dos volumes finitos baseado no artigo  (Sheng e Yuan, 2011)
-% nlfvDMPSY --> método não linear que preserva DMP baseado no artigo (Gao e Wu, 2013) e (Sheng e Yuan, 20...)
-% lfvLPEW --> método linear based no método não linear usando LPEW;
-% lfvHP --> método linear baseado no método não linear usando pontos
-% harmônicos 
+%% Defina o tipo de solver de pressao
+% mpfad--> metodo linear dos volumes finitos baseado no Gao e Wu 2010
+% tpfa ---> metodo Linear dos volumes finito
+% nlfvLPEW --> metodo nao linear dos volumes finitos baseado no artigo  (Sheng e Yuan, 2011)
+% nlfvDMPSY --> metodo nao linear que preserva DMP baseado no artigo (Gao e Wu, 2013) e (Sheng e Yuan, 20...)
+% lfvLPEW --> metodo linear based no metodo nao linear usando LPEW;
+% lfvHP --> metodo linear baseado no metodo nao linear usando pontos
+% harmonicos
 pmetodo='mpfad';
 %pmetodo='nlfvLPEW';
-%% metodo de interação: iterpicard, iternewton, iterbroyden, itersecant,
+%% metodo de interacao: iterpicard, iternewton, iterbroyden, itersecant,
 %iterfreejacobian,iterdiscretnewton, JFNK
 %iteration='iterdiscretnewton';
 %iteration='iterpicard';
 %iteration='iterbroyden';
 iteration='iterpicard';
 %iteration='iterhybrid';
-%% defina o ponto de interpolação
+%% defina o ponto de interpolacao
 interpol='LPEW2';
-%% Defina o tipo de solver de saturação
+%% Defina o tipo de solver de saturacao
 %1. escreve ['FOU'] se deseja executar FOU method
-%2. escreve 'HOFV-E' se deseja executar método de alta por simple
-%extrapolação
-%3. escreve 'HOMFV' se deseja executar método de alta ordem com limitador
-%de nó, na esqueça de definir upsilon (default é 0.2 como indica o artigo)
-%2. escreve ['GOE'] se deseja executar o método GOE-free;
+%2. escreve 'HOFV-E' se deseja executar metodo de alta por simple
+%extrapolacao
+%3. escreve 'HOMFV' se deseja executar metodo de alta ordem com limitador
+%de no, na esqueca de definir upsilon (default e 0.2 como indica o artigo)
+%2. escreve ['GOE'] se deseja executar o metodo GOE-free;
 smetodo='FOU';
 order=1;
 %% ordem de Runge Kutta
 tordem=1;
-% uso especial quando esta ativado o limitador de Woodfield, forneça um
+% uso especial quando esta ativado o limitador de Woodfield, forneca um
 % valor no intervalo [0,1]
 upsilon=0.2;
 % Quando
-% kapp=0--> método de Fromm
-% kappa=1--> método de diferenças centradas com três pontos
-% kappa=-1--> método de ponderação a montante de segunda ordem
-% kappa=1/3--> método de ponderação a montante de terceira ordem
+% kapp=0--> metodo de Fromm
+% kappa=1--> metodo de diferencas centradas com tres pontos
+% kappa=-1--> metodo de ponderacao a montante de segunda ordem
+% kappa=1/3--> metodo de ponderacao a montante de terceira ordem
 kappa=1/3;
 % digite segundo o benchmark
 % benchmark
@@ -141,25 +141,25 @@ kappa=1/3;
 % shuec
 % buckley
 benchmark='durlofsky';
-% nome do arquivo é unico para cada exemplo
+% nome do arquivo  unico para cada exemplo
 namefile='Report_Production_Mesh_lamine_LFVHP.dat';
 % escreve sobre o arquivo criado
 fid3 = fopen(namefile,'w');
 
-%% adequação dos flags
+%% adequacao dos flags
 %nflag= calflag(pmetodo);
-%% este flag só use quando o problema é Buckley-Leverett com fluxo imposto
-% na face com flag 202, porém a saturação será imposto na face
+%% este flag se use quando o problema e Buckley-Leverett com fluxo imposto
+% na face com flag 202, porm a saturacao sera imposto na face
 auxflag=202;
-%% adequação das permeabilidades segundo o bechmark
+%% adequacao das permeabilidades segundo o bechmark
 [elem,kmap,normKmap,solanal,bedge,fonte,velanal]=adequapermeab(benchmark, kmap,elem,bedge);
 sat=ones(size(elem,1),1);
 postprocessor(solanal,sat,1);
 
-%% pre-precessador-saturação
+%% pre-precessador-saturacao
 [N,F,V,weightLS,esuel1,esuel_coord,A,bound,S_old,S_cont]=presaturation(wells);
 
-%% pre-método-multidimensional
+%% pre-metodo-multidimensional
 %[elemphant,elembedge,face_bedge,peso,nu]=premultidimensional(N);
 
 %% pre-metodo-nao-linear
@@ -168,7 +168,7 @@ postprocessor(solanal,sat,1);
 nflag=nflagno;
 %[aroundface]=aroundfacelement(F,pointarmonic);
 %% IMPES
-% inicializando as variáveis
+% inicializando as variaveis
 cont = 1;
 vpi_old = 0;
 VPI(1) = 0;
@@ -182,43 +182,43 @@ time2=0;
 
 while vpi_old < vpi
 %while t_old<totaltime(2)
-    %% calculo das mobilidades 
+    %% calculo das mobilidades
     [mobility] = mobilityface(S_old,nw,no,auxflag,S_cont,simu);
-    
-    %%  Calculo da Pressão Implicita pelos método lineares e não-lineares
+
+    %%  Calculo da Pressao Implicita pelos matodo lineares e nao-lineares
     [p,errorelativo,flowrate,flowresult]=solverpressure(kmap,nflagface,nflagno,fonte, tol,...
         nit,p_old,mobility,gamma,wells,S_old,V,nw,no,N,parameter,...
         pmetodo,auxflag,interptype,Hesq, Kde, Kn, Kt, Ded,weightDMP,auxface,benchmark,iteration,nflag);
-        
-    %% quando a velocidade é unitario
+
+    %% quando a velocidade e unitario
     %[p,influx,q] = getknownflowrate;
     %% calculo do fluxo fracional em cada elemento da malha
     f_elem = fractionalflow(S_old,nw,no);
-    
+
     %% caculo do passo de tempo
     d_t=steptime(f_elem,S_old,flowrate,CFL,S_cont,auxflag,nw,no,order);
-    
-    %% calculo da saturação explicito
+
+    %% calculo da saturacao explicito
     S_old=solversaturation(S_old,flowrate,d_t,esuel1,wells,flowresult,...
         f_elem,S_cont,weightLS,bound,smetodo,tordem,...
         upsilon,kappa,nw,no,auxflag);
-        
-    %% reporte de produção
+
+    %% reporte de producao
     [VPI,oilrecovery,cumulateoil,watercut]=reportproduction(S_old,...
         wells,f_elem,cont,VPI,oilrecovery,cumulateoil,watercut,flowresult,d_t);
-    
+
     %% calculo o passo de vpi ou passo de tempo
     %t_old=VPI
-    
+
     vpi_old=VPI(cont)
-   
-    %visualização
+
+    %visualizacao
     if vpi_old >= time2
        step = 1000*time2;
        postprocessor(p,S_old,step)
       time2 = time2 + 0.01;
     end
-    % armezena a produção em arquivo .dat
+    % armezena a producao em arquivo .dat
     fprintf(fid3,'%13.3e %13.3e %13.3e %13.3e \n',VPI(cont), ...
        oilrecovery(cont),cumulateoil(cont),watercut(cont));
     vpi_old=1e+40
@@ -228,7 +228,7 @@ end
 fclose(fid3);
 %% calculo do erro
 [erropressure,errovelocity]=errorateconv(solanal, p, velanal,flowrate,erromethod)
-%% calculo das pressões máximas e minimas
+%% calculo das pressoes maximas e minimas
 panalmax= max(p)
 panalmin= min(p)
 
@@ -238,4 +238,3 @@ toc
 % plotBL(S_old,h)
 % end
 % S_old;
-
